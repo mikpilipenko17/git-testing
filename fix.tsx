026646b7b4fdb19fce49b200b47c3824c77f7ec6
Pilipenko Kirill <kirillpilipenko061@gmail.com>
2021-06-08 17:11:15 +0300

    added typescript file

---
+++ fix.tsx
@@ -0,0 +1,16 @@
+enum GENRE {
+    ROMANTIC,
+    DRAMA,
+    COMEDY,
+    DOCUMENTARY,
+  }
+  
+  projector.configureFilm(GENRE.COMEDY);
+  
+  class Projector {
+    configureFilm(genre) {
+      switch (genre) {
+        case GENRE.ROMANTIC:
+      }
+    }
+  }
